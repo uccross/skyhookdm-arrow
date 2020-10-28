4eade22595a127adab70152072d10274c595e78d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-10-28 16:38:29 +0530

    update build-and-test.sh

--- cpp/src/arrow/adapters/skyhookdm-ceph-cls/scripts/build-and-test.sh
+++ cpp/src/arrow/adapters/skyhookdm-ceph-cls/scripts/build-and-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp ../../../../debug1/debug/libcls_arrow* /usr/lib64/rados-classes/
-cp ../../../../debug1/debug/libarrow* /usr/lib64/rados-classes/
+cp ../../../../debug/debug/libcls_arrow* /usr/lib64/rados-classes/
 scripts/micro-osd.sh test-cluster /etc/ceph
 build/bin/cls_arrow_test
