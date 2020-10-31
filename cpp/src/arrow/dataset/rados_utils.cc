2e300229bba28940e6416284e6cdadfecc0854cf
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-10-31 21:40:40 +0530

    update bl api

--- cpp/src/arrow/dataset/rados_utils.cc
+++ cpp/src/arrow/dataset/rados_utils.cc
@@ -61,19 +61,24 @@ Status deserialize_scan_request_from_bufferlist(std::shared_ptr<Expression>* fil
                                                 librados::bufferlist& bl) {
   int64_t filter_size = 0;
   char filter_size_buffer[8];
-  bl.begin(0).copy(8, filter_size_buffer);
+
+  librados::bufferlist::iterator itr(&bl);
+  itr.copy(8, filter_size_buffer);
   ARROW_RETURN_NOT_OK(char_to_int64((uint8_t*)filter_size_buffer, filter_size));
+  itr += 8;
 
   char* filter_buffer = new char[filter_size];
-  bl.begin(8).copy(filter_size, filter_buffer);
+  itr.copy(filter_size, filter_buffer);
+  itr += filter_size;
 
   int64_t schema_size = 0;
   char schema_size_buffer[8];
-  bl.begin(8 + filter_size).copy(8, schema_size_buffer);
+  itr.copy(8, schema_size_buffer);
   ARROW_RETURN_NOT_OK(char_to_int64((uint8_t*)schema_size_buffer, schema_size));
+  itr += 8;
 
   char* schema_buffer = new char[schema_size];
-  bl.begin(16 + filter_size).copy(schema_size, schema_buffer);
+  itr.copy(schema_size, schema_buffer);
 
   ARROW_ASSIGN_OR_RAISE(auto filter_, Expression::Deserialize(
                                           Buffer((uint8_t*)filter_buffer, filter_size)));
