ca06d29d0eeb18b552853f2d9544304c23569125
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-11-04 14:44:38 +0530

    fix integration ceph script

--- ci/scripts/integration_ceph.sh
+++ ci/scripts/integration_ceph.sh
@@ -18,45 +18,41 @@
 # under the License.
 
 set -e
-set -u
-set -x
+
+source_dir=${1}/cpp
+build_dir=${2}/cpp
+test_dir=${build_dir}/test-cluster
 
 pushd ${build_dir}
-
-TEST_DIR=$1
-CONF_DIR=$2
-echo "TEST_DIR=${TEST_DIR}"
-echo "CONF_DIR=${CONF_DIR}"
-echo "pwd="`pwd`
 
 # get rid of process and directories leftovers
 pkill ceph-mon || true
 pkill ceph-osd || true
-rm -fr $TEST_DIR
+rm -fr ${test_dir}
 
 # cluster wide parameters
-mkdir -p ${TEST_DIR}/log
-cat >> $CONF_DIR/ceph.conf <<EOF
+mkdir -p ${test_dir}/log
+cat >> /etc/ceph/ceph.conf <<EOF
 [global]
 fsid = $(uuidgen)
 osd crush chooseleaf type = 0
-run dir = ${TEST_DIR}/run
+run dir = ${test_dir}/run
 auth cluster required = none
 auth service required = none
 auth client required = none
 osd pool default size = 1
 EOF
-export CEPH_ARGS="--conf ${CONF_DIR}/ceph.conf"
+export CEPH_ARGS="--conf /etc/ceph/ceph.conf"
 
 # single monitor
-MON_DATA=${TEST_DIR}/mon
+MON_DATA=${test_dir}/mon
 mkdir -p $MON_DATA
 
-cat >> $CONF_DIR/ceph.conf <<EOF
+cat >> /etc/ceph/ceph.conf <<EOF
 [mon.0]
-log file = ${TEST_DIR}/log/mon.log
+log file = ${test_dir}/log/mon.log
 chdir = ""
-mon cluster log file = ${TEST_DIR}/log/mon-cluster.log
+mon cluster log file = ${test_dir}/log/mon-cluster.log
 mon data = ${MON_DATA}
 mon addr = 127.0.0.1
 # this was added to enable pool deletion within method delete_one_pool_pp()
@@ -68,12 +64,12 @@ touch ${MON_DATA}/keyring
 ceph-mon --id 0
 
 # single osd
-OSD_DATA=${TEST_DIR}/osd
+OSD_DATA=${test_dir}/osd
 mkdir ${OSD_DATA}
 
-cat >> $CONF_DIR/ceph.conf <<EOF
+cat >> /etc/ceph/ceph.conf <<EOF
 [osd.0]
-log file = ${TEST_DIR}/log/osd.log
+log file = ${test_dir}/log/osd.log
 chdir = ""
 osd data = ${OSD_DATA}
 osd journal = ${OSD_DATA}.journal
@@ -90,10 +86,10 @@ ceph-osd --id ${OSD_ID}
 # check that it works
 ceph osd pool create rbd 8 8
 rados --pool rbd put group /etc/group
-rados --pool rbd get group ${TEST_DIR}/group
-diff /etc/group ${TEST_DIR}/group
+rados --pool rbd get group ${test_dir}/group
+diff /etc/group ${test_dir}/group
 ceph osd tree
 
-export CEPH_CONF="${CONF_DIR}/ceph.conf"
+export CEPH_CONF="/etc/ceph/ceph.conf"
 cp debug/libcls_arrow* /usr/lib64/rados-classes/
 popd
