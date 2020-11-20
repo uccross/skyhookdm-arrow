c96f0a605f031794da89af4595c6a01b05b45318
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-11-20 20:46:41 +0530

    fix linting problems

--- cpp/src/arrow/dataset/dataset_rados.cc
+++ cpp/src/arrow/dataset/dataset_rados.cc
@@ -96,8 +96,8 @@ Result<std::shared_ptr<DatasetFactory>> RadosDatasetFactory::Make(
 Result<std::vector<std::shared_ptr<Schema>>> RadosDatasetFactory::InspectSchemas(
     InspectOptions options) {
   librados::bufferlist in, out;
-  int e = rados_options_->io_ctx_interface_->exec(objects_[0]->id(),
-      rados_options_->cls_name_.c_str(), "read", in, out);
+  int e = rados_options_->io_ctx_interface_->exec(
+      objects_[0]->id(), rados_options_->cls_name_.c_str(), "read", in, out);
   if (e != 0) {
     return Status::ExecutionError("call to exec() returned non-zero exit code.");
   }
