99e7971478494e8b3e9b9aaf06adeadf8ed1dab9
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-01-12 20:30:05 +0530

    install unzip

--- ci/scripts/integration_ceph.sh
+++ ci/scripts/integration_ceph.sh
@@ -100,6 +100,7 @@ ceph osd pool create test-pool 32 32 replicated
 
 # download and write the partitioned nyctaxi dataset in parquet format
 wget -O nyc.zip https://github.com/JayjeetAtGithub/zips/blob/main/nyc.zip?raw=true
+apt install -y unzip
 unzip nyc.zip
 wget https://github.com/JayjeetAtGithub/zips/raw/main/ppw.py
 python3 ppw.py
