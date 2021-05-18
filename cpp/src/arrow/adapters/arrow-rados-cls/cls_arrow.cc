09f6d04e06ba2bd4c57ea145119fb5f1193ac5a1
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-19 00:40:54 +0530

    updates

--- cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
+++ cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
@@ -162,8 +162,8 @@ cls_method_handle_t h_scan_op;
 static int scan_op(cls_method_context_t hctx, ceph::bufferlist* in,
                    ceph::bufferlist* out) {
   // the components required to construct a ParquetFragment.
-  arrow::dataset::Expression filter;
-  arrow::dataset::Expression partition_expression;
+  // arrow::dataset::Expression filter;
+  // arrow::dataset::Expression partition_expression;
   std::shared_ptr<arrow::Schema> projection_schema;
   std::shared_ptr<arrow::Schema> dataset_schema;
   int64_t file_size;
