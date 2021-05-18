24f2b88798fd73bcad8121fe487cb9595ca4e5be
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-19 00:36:27 +0530

    debug cls

--- cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
+++ cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
@@ -166,7 +166,7 @@ static int scan_op(cls_method_context_t hctx, ceph::bufferlist* in,
   // arrow::dataset::Expression filter;
   // arrow::dataset::Expression partition_expression;
   // std::shared_ptr<arrow::Schema> projection_schema;
-  // std::shared_ptr<arrow::Schema> dataset_schema;
+  std::shared_ptr<arrow::Schema> dataset_schema;
   int64_t file_size;
 
   // // // deserialize the scan request
