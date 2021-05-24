eb77c82c35919579839f940fc0ba8181bd04ffe0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-25 00:49:56 +0530

    try fixing memory leak in cls

--- cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
+++ cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
@@ -40,6 +40,7 @@ class RandomAccessObject : public arrow::io::RandomAccessFile {
   explicit RandomAccessObject(cls_method_context_t hctx, int64_t file_size) {
     hctx_ = hctx;
     content_length_ = file_size;
+    chunks_ = std::vector<std::shared_ptr<ceph::bufferlist>>();
   }
 
   arrow::Status CheckClosed() const {
@@ -69,8 +70,9 @@ class RandomAccessObject : public arrow::io::RandomAccessFile {
     nbytes = std::min(nbytes, content_length_ - position);
 
     if (nbytes > 0) {
-      ceph::bufferlist* bl = new ceph::bufferlist();
-      cls_cxx_read(hctx_, position, nbytes, bl);
+      std::shared_ptr<ceph::bufferlist> bl = std::make_shared<ceph::bufferlist>();
+      cls_cxx_read(hctx_, position, nbytes, bl.get());
+      chunks_.push_back(bl);
       return std::make_shared<arrow::Buffer>((uint8_t*)bl->c_str(), bl->length());
     }
     return std::make_shared<arrow::Buffer>("");
@@ -108,6 +110,9 @@ class RandomAccessObject : public arrow::io::RandomAccessFile {
 
   arrow::Status Close() {
     closed_ = true;
+    for (auto chunk: chunks_) {
+      delete chunk;
+    }
     return arrow::Status::OK();
   }
 
@@ -118,6 +123,7 @@ class RandomAccessObject : public arrow::io::RandomAccessFile {
   bool closed_ = false;
   int64_t pos_ = 0;
   int64_t content_length_ = -1;
+  std::vector<std::shared_ptr<ceph::bufferlist>> chunks_;
 };
 
 static arrow::Status ScanParquetObject(cls_method_context_t hctx,
