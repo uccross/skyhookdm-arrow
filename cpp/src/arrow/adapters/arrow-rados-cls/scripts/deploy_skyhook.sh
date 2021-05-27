9c6bd64c8886f71847ea88ed4fb4575b1cc5f996
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-27 12:41:44 +0530

    updates

--- cpp/src/arrow/adapters/arrow-rados-cls/scripts/deploy_skyhook.sh
+++ cpp/src/arrow/adapters/arrow-rados-cls/scripts/deploy_skyhook.sh
@@ -38,16 +38,24 @@ cd cpp/debug
 cmake -DCMAKE_BUILD_TYPE=Debug -DARROW_CLS=ON -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON -DARROW_WITH_ZLIB=ON -DARROW_BUILD_EXAMPLES=ON -DPARQUET_BUILD_EXAMPLES=ON -DARROW_PYTHON=ON -DARROW_DATASET=ON -DARROW_CSV=ON -DARROW_WITH_LZ4=ON -DARROW_WITH_ZSTD=ON ..
 make -j4 install
 
+export WORKDIR=${WORKDIR:-$HOME}
+export ARROW_HOME=$WORKDIR/dist
 export PYARROW_BUILD_TYPE=Debug
 export PYARROW_WITH_DATASET=1
 export PYARROW_WITH_PARQUET=1
 export PYARROW_WITH_RADOS=1
 
+mkdir -p /root/dist/lib
+mkdir -p /root/dist/include
+
+cp -r /usr/local/lib/. /root/dist/lib
+cp -r /usr/local/include/. /root/dist/include
+
 cd /tmp/arrow/python
 pip3 install -r requirements-build.txt -r requirements-test.txt
 pip3 install wheel
-python3 setup.py build_ext --inplace --bundle-arrow-cpp bdist_wheel
 rm -rf dist/*
+python3 setup.py build_ext --inplace --bundle-arrow-cpp bdist_wheel
 pip3 install --upgrade dist/*.whl
 
 cd /tmp/arrow/cpp/debug/debug
