097f065228e33910b67d3a032c7ce34720cfecad
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-05-30 14:29:41 +0530

    updates

--- cpp/src/arrow/dataset/file_rados_parquet_test.cc
+++ cpp/src/arrow/dataset/file_rados_parquet_test.cc
@@ -1,3 +1,7 @@
+#include "arrow/api.h"
+#include "arrow/compute/exec/expression.h"
+#include "arrow/dataset/scanner.h"
+#include "arrow/dataset/file_rados_parquet.h"
 #include "arrow/dataset/test_util.h"
 
 namespace arrow {
@@ -6,7 +10,15 @@ namespace dataset {
 TEST(TestRadosParquetFileFormat, SerializeDeserializeRoundTrip) {
     std::shared_ptr<ScanOptions> options;
     ceph::bufferlist bl;
-    ASSERT_OK(SerializeScanRequest(options, 100, bl));
+    int64_t file_size = 1000000;
+    ASSERT_OK(SerializeScanRequest(options, file_size, bl));
+
+    compute::Expression filter_;
+    compute::Expression partition_expression_;
+    std::shared_ptr<Schema> projected_schema;
+    std::shared_ptr<Schema> dataset_schema_;
+    int64_t file_size_;
+    ASSERT_OK(DeserializeScanRequest(filter_, partition_expression_, projected_schema, dataset_schema_, file_size_, bl);
 }
 
 } // namespace dataset
