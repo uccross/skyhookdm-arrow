4dc13a7188ef0b17e2b8a68ddf3be11dc787d95f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-06-24 01:47:27 +0530

    updates

--- cpp/src/arrow/adapters/arrow-rados-cls/scripts/deploy_ceph.sh
+++ cpp/src/arrow/adapters/arrow-rados-cls/scripts/deploy_ceph.sh
@@ -18,18 +18,20 @@
 # under the License.
 set -eu
 
-if [[ $# -lt 4 ]] ; then
-    echo "usage: ./deploy_ceph.sh [mon hosts] [osd hosts] [mds hosts] [mgr hosts] [blkdevice]"
+if [[ $# -lt 6 ]] ; then
+    echo "usage: ./deploy_ceph.sh [mon hosts] [osd hosts] [mds hosts] [mgr hosts] [blkdevice] [pool size]"
     echo " "
-    echo "for example: ./deploy_ceph.sh node1,node2,node3 node4,node5,node6 node1 node1 /dev/sdb"
+    echo "for example: ./deploy_ceph.sh node1,node2,node3 node4,node5,node6 node1 node1 /dev/sdb 3"
     exit 1
 fi
 
-MON=$1
-OSD=$2
-MDS=$3
-MGR=$4
-BLKDEV=$5
+# in default mode (without any arguments), deploy a single node Ceph cluster"
+MON=${1:-node1}
+OSD=${2:-node1}
+MDS=${3:-node1}
+MGR=${4:-node1}
+BLKDEV=${5:-/dev/nvme0n1p4}
+POOL_SIZE=${6:-1}
 
 IFS=',' read -ra MON_LIST <<< "$MON"; unset IFS
 IFS=',' read -ra OSD_LIST <<< "$OSD"; unset IFS
@@ -51,28 +53,30 @@ rm -rf /etc/ceph/*
 rm -rf /tmp/deployment
 rm -rf /tmp/ceph-deploy
 for node in ${OSD_LIST}; do
-    ssh $node rm -rf /etc/ceph/ceph.conf
+    ssh $node rm -rf /etc/ceph/*
+    # try to zap the block devices.
+    ssh $node ceph-volume lvm zap $BLKDEV --destroy || true
 done
 for node in ${MON_LIST}; do
-    ssh $node rm -rf /etc/ceph/ceph.conf
+    ssh $node rm -rf /etc/ceph/*
 done
 for node in ${MGR_LIST}; do
-    ssh $node rm -rf /etc/ceph/ceph.conf
+    ssh $node rm -rf /etc/ceph/*
 done
 for node in ${MDS_LIST}; do
-    ssh $node rm -rf /etc/ceph/ceph.conf
+    ssh $node rm -rf /etc/ceph/*
 done
 
 echo "[1] installing common packages"
 apt update
-apt install -y python3-venv python3-pip ceph-fuse ceph-common
+apt install -y python3-venv python3-pip ceph-fuse ceph-common attr
 
 echo "[2] installing ceph-deploy"
 git clone https://github.com/ceph/ceph-deploy /tmp/ceph-deploy
 pip3 install --upgrade /tmp/ceph-deploy
 
 mkdir /tmp/deployment
-cd /tmp/deployment/
+cd /tmp/deployment
 
 echo "[3] initializng Ceph config"
 ceph-deploy new $MON_LIST
@@ -97,7 +101,10 @@ ceph-deploy --overwrite-conf config push $MON_LIST $OSD_LIST $MDS_LIST $MGR_LIST
 
 cp ceph.conf /etc/ceph/ceph.conf
 cp ceph.client.admin.keyring  /etc/ceph/ceph.client.admin.keyring
+
+# pause and let user's can take a quick look if everything is fine before deploying OSDs
 ceph -s
+sleep 5
 
 echo "[7] deploying OSDs"
 for node in ${OSD_LIST}; do
@@ -112,7 +119,14 @@ ceph-deploy mds create $MDS_LIST
 echo "[9] creating pools for deploying CephFS"
 ceph osd pool create cephfs_data 128
 ceph osd pool create cephfs_metadata 16
+
+# turn off pg autoscale
 ceph osd pool set cephfs_data pg_autoscale_mode off
+
+# set the pool sizes based on commandline arguments
+ceph osd pool set cephfs_data size $POOL_SIZE
+ceph osd pool set cephfs_metadata size $POOL_SIZE
+ceph osd pool set device_health_metrics size $POOL_SIZE
 
 echo "[9] deploying CephFS"
 ceph fs new cephfs cephfs_metadata cephfs_data
@@ -122,5 +136,5 @@ echo "[10] mounting CephFS at /mnt/cephfs"
 sleep 5
 ceph-fuse /mnt/cephfs
 
-echo "Done."
+echo "Ceph deployed successfully !"
 ceph -s
