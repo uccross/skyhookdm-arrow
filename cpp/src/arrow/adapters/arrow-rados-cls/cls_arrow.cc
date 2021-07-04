df21f7311821be4466c396166039c8a4bc24d021
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-04 10:42:35 +0530

    fixes

--- cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
+++ cpp/src/arrow/adapters/arrow-rados-cls/cls_arrow.cc
@@ -147,7 +147,7 @@ class RandomAccessObject : public arrow::io::RandomAccessFile {
 /// \param[in] partition_expression The partition expression to use.
 /// \param[in] projection_schema The projection schema.
 /// \param[in] dataset_schema The dataset schema.
-/// \param[out] table Table to store the resultant data.
+/// \param[out] result_table Table to store the resultant data.
 /// \param[in] object_size The size of the object.
 /// \return Status.
 static arrow::Status ScanIpcObject(cls_method_context_t hctx,
@@ -155,7 +155,7 @@ static arrow::Status ScanIpcObject(cls_method_context_t hctx,
                                    arrow::dataset::Expression partition_expression,
                                    std::shared_ptr<arrow::Schema> projection_schema,
                                    std::shared_ptr<arrow::Schema> dataset_schema,
-                                   std::shared_ptr<arrow::Table>& t,
+                                   std::shared_ptr<arrow::Table>& result_table,
                                    int64_t object_size) {
   auto file = std::make_shared<RandomAccessObject>(hctx, object_size);
   arrow::dataset::FileSource source(file, arrow::util::Compression::LZ4_FRAME);
@@ -164,9 +164,9 @@ static arrow::Status ScanIpcObject(cls_method_context_t hctx,
   ARROW_ASSIGN_OR_RAISE(auto fragment,
                         format->MakeFragment(source, partition_expression));
 
-  auto ctx = std::make_shared<arrow::dataset::ScanContext>();
+  auto options = std::make_shared<arrow::dataset::ScanOptions>();
   auto builder =
-      std::make_shared<arrow::dataset::ScannerBuilder>(dataset_schema, fragment, ctx);
+      std::make_shared<arrow::dataset::ScannerBuilder>(dataset_schema, fragment, options);
 
   ARROW_RETURN_NOT_OK(builder->Filter(filter));
   ARROW_RETURN_NOT_OK(builder->Project(projection_schema->field_names()));
@@ -175,7 +175,7 @@ static arrow::Status ScanIpcObject(cls_method_context_t hctx,
   ARROW_ASSIGN_OR_RAISE(auto scanner, builder->Finish());
   ARROW_ASSIGN_OR_RAISE(auto table, scanner->ToTable());
 
-  t = table;
+  result_table = table;
 
   ARROW_RETURN_NOT_OK(file->Close());
   return arrow::Status::OK();
@@ -187,7 +187,7 @@ static arrow::Status ScanIpcObject(cls_method_context_t hctx,
 /// \param[in] partition_expression The partition expression to use.
 /// \param[in] projection_schema The projection schema.
 /// \param[in] dataset_schema The dataset schema.
-/// \param[out] table Table to store the resultant data.
+/// \param[out] result_table Table to store the resultant data.
 /// \param[in] object_size The size of the object.
 /// \return Status.
 static arrow::Status ScanParquetObject(cls_method_context_t hctx,
@@ -198,7 +198,6 @@ static arrow::Status ScanParquetObject(cls_method_context_t hctx,
                                        std::shared_ptr<arrow::Table>& result_table,
                                        int64_t object_size) {
   auto file = std::make_shared<RandomAccessObject>(hctx, object_size);
-
   arrow::dataset::FileSource source(file);
 
   auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
