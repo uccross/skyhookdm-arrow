d3576a65935df78733087e3710820c7ff386541b
root <root@localhost>
2021-07-27 07:04:38 -0600

    updates

--- cpp/src/jni/dataset/jni_wrapper.cc
+++ cpp/src/jni/dataset/jni_wrapper.cc
@@ -84,7 +84,8 @@ arrow::Result<std::shared_ptr<arrow::dataset::FileFormat>> GetFileFormat(
       return std::make_shared<arrow::dataset::ParquetFileFormat>();
     case 1:
       return std::make_shared<arrow::dataset::SkyhookFileFormat>(
-        "parquet", "/etc/ceph/ceph.conf", "cephfs_data", "client.admin", "ceph", "arrow");
+          "parquet", "/etc/ceph/ceph.conf", "cephfs_data", "client.admin", "ceph",
+          "arrow");
     default:
       std::string error_message =
           "illegal file format id: " + std::to_string(file_format_id);
