c0d6149c74bd76481993ce45693b17921c4b0d67
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-08-04 13:55:30 -0600

    Add Skyhook C++ code

--- cpp/src/arrow/dataset/scanner_internal.h
+++ cpp/src/arrow/dataset/scanner_internal.h
@@ -185,6 +185,10 @@ inline Result<ScanTaskIterator> GetScanTaskIterator(
   auto fn = [options](std::shared_ptr<Fragment> fragment) -> Result<ScanTaskIterator> {
     ARROW_ASSIGN_OR_RAISE(auto scan_task_it, fragment->Scan(options));
 
+    if (options->skip_compute) {
+      return std::move(scan_task_it);
+    }
+
     auto partition = fragment->partition_expression();
     // Apply the filter and/or projection to incoming RecordBatches by
     // wrapping the ScanTask with a FilterAndProjectScanTask
