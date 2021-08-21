2dcd5778e767a9624deed0f45553ee2dca5681ff
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-08-21 22:16:12 +0530

    updates

--- cpp/src/skyhook/protocol/rados_protocol.cc
+++ cpp/src/skyhook/protocol/rados_protocol.cc
@@ -29,26 +29,26 @@ RadosStatus GetStatusFromReturnCode(int code, std::string msg) {
 
 RadosStatus IoCtxWrapper::read(const std::string& oid, ceph::bufferlist& bl, size_t len,
                                uint64_t offset) {
-  return GetStatusFromReturnCode(this->ioCtx->read(oid, bl, len, offset),
+  return GetStatusFromReturnCode(ioCtx->read(oid, bl, len, offset),
                                  "ioctx->read failed.");
 }
 
 RadosStatus IoCtxWrapper::exec(const std::string& oid, const char* cls,
                                const char* method, ceph::bufferlist& in,
                                ceph::bufferlist& out) {
-  return GetStatusFromReturnCode(this->ioCtx->exec(oid, cls, method, in, out),
+  return GetStatusFromReturnCode(ioCtx->exec(oid, cls, method, in, out),
                                  "ioctx->exec failed.");
 }
 
 RadosStatus IoCtxWrapper::stat(const std::string& oid, uint64_t* psize) {
-  return GetStatusFromReturnCode(this->ioCtx->stat(oid, psize, NULL),
+  return GetStatusFromReturnCode(ioCtx->stat(oid, psize, NULL),
                                  "ioctx->stat failed.");
 }
 
 std::vector<std::string> IoCtxWrapper::list() {
   std::vector<std::string> oids;
-  librados::NObjectIterator begin = this->ioCtx->nobjects_begin();
-  librados::NObjectIterator end = this->ioCtx->nobjects_end();
+  librados::NObjectIterator begin = ioCtx->nobjects_begin();
+  librados::NObjectIterator end = ioCtx->nobjects_end();
   for (; begin != end; begin++) {
     oids.push_back(begin->get_oid());
   }
@@ -57,27 +57,27 @@ std::vector<std::string> IoCtxWrapper::list() {
 
 RadosStatus RadosWrapper::init2(const char* const name, const char* const clustername,
                                 uint64_t flags) {
-  return GetStatusFromReturnCode(this->cluster->init2(name, clustername, flags),
+  return GetStatusFromReturnCode(cluster->init2(name, clustername, flags),
                                  "rados->init failed.");
 }
 
 RadosStatus RadosWrapper::ioctx_create(const char* name, IoCtxInterface* pioctx) {
   librados::IoCtx ioCtx;
-  int ret = this->cluster->ioctx_create(name, ioCtx);
+  int ret = cluster->ioctx_create(name, ioCtx);
   pioctx->setIoCtx(&ioCtx);
   return GetStatusFromReturnCode(ret, "rados->ioctx_create failed.");
 }
 
 RadosStatus RadosWrapper::conf_read_file(const char* const path) {
-  return GetStatusFromReturnCode(this->cluster->conf_read_file(path),
+  return GetStatusFromReturnCode(cluster->conf_read_file(path),
                                  "rados->conf_read_file failed.");
 }
 
 RadosStatus RadosWrapper::connect() {
-  return GetStatusFromReturnCode(this->cluster->connect(), "rados->connect failed.");
+  return GetStatusFromReturnCode(cluster->connect(), "rados->connect failed.");
 }
 
-void RadosWrapper::shutdown() { this->cluster->shutdown(); }
+void RadosWrapper::shutdown() { cluster->shutdown(); }
 
 RadosConn::~RadosConn() { Shutdown(); }
 
