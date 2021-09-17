15091de04a2c6439a2f8cd01311964162b829407
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-09-17 14:18:12 -0700

    Alphabetical ordering

--- cpp/cmake_modules/DefineOptions.cmake
+++ cpp/cmake_modules/DefineOptions.cmake
@@ -224,8 +224,6 @@ if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
 
   define_option(ARROW_GANDIVA "Build the Gandiva libraries" OFF)
 
-  define_option(ARROW_SKYHOOK "Build the Skyhook libraries" OFF)
-
   define_option(ARROW_HDFS "Build the Arrow HDFS bridge" OFF)
 
   define_option(ARROW_HIVESERVER2 "Build the HiveServer2 client and Arrow adapter" OFF)
@@ -261,6 +259,8 @@ if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
   define_option(ARROW_PYTHON "Build the Arrow CPython extensions" OFF)
 
   define_option(ARROW_S3 "Build Arrow with S3 support (requires the AWS SDK for C++)" OFF)
+
+  define_option(ARROW_SKYHOOK "Build the Skyhook libraries" OFF)
 
   define_option(ARROW_TENSORFLOW "Build Arrow with TensorFlow support enabled" OFF)
 
